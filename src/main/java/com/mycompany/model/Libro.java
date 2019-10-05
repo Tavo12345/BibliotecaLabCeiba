adf667dadcc28b08b60370dac777d22071a675a9
Tavo12345 <gust.allg@gmail.com>
2019-10-05 11:02:11 -0500

    Creando paquetes

---
+++ src/main/java/com/mycompany/model/Libro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Gustavo
+ */
+@Entity
+@Table(name="libro")
+public class Libro {
+    
+}
