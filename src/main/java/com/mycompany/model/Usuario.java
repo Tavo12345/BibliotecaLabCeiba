cf00f3b75e490466ea565b8f860dcd83a04ca1b9
cdgparedes <cdgparedes88@gmail.com>
2019-10-05 16:56:42 -0500

    actualizando vista libros y contra usurio

---
+++ src/main/java/com/mycompany/model/Usuario.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */ 
+package com.mycompany.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Cesar
+ */
+@Entity
+@Table(name="usuario")
+public class Usuario {
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private String cedula;
+    
+    @Column(name="nombre")
+    private String nombre;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="telefono")
+    private String telefono;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+}
