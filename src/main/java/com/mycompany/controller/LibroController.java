632c56649a84c73b9d1fd5971b4dc2c3ffc88a66
Tavo12345 <gust.allg@gmail.com>
2019-10-05 21:31:23 -0500

    Controladores y persistencia

---
+++ src/main/java/com/mycompany/controller/LibroController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.controller;
+
+import com.mycompany.ejb.LibroFacadeLocal;
+import com.mycompany.model.Libro;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Gustavo
+ */
+@Named
+@ViewScoped
+public class LibroController implements Serializable {
+    @EJB
+    private LibroFacadeLocal libroEJB;
+    private Libro libro;
+    
+    @PostConstruct
+    public void init(){
+        libro = new Libro();
+    }
+    
+    public void registrar(){
+        try{
+            libroEJB.create(libro);
+        }catch(Exception e){
+            //Mensaje pendiente por definir
+        }
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+    
+}
